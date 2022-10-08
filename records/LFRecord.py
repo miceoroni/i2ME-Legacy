828247480a1ad702312af96a28106e83a777c73f
April <mewtek@proton.me>
2022-10-08 00:59:39 -0700

    LFRecord DB and boilerplate code for a handler.

---
+++ records/LFRecord.py
@@ -0,0 +1,10 @@
+import sqlite3
+
+
+""" General Handler for quickly grabbing resources pertaining to LFRecords. """
+
+def getZip(locId: str):
+    pass
+
+def getLocationInfo(locId: str):
+    pass
