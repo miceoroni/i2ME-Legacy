a33ad3367881770891414b10caf1a7cc64f9d3f0
April <mewtek@proton.me>
2022-10-13 01:36:18 -0700

    Very rudamentary implementation of async tasks in main.py

--- main.py
+++ main.py
@@ -1,10 +1,35 @@
 import asyncio
+# from re import A
+from recordGenerators import DailyForecast,CurrentObservations,HourlyForecast,AirQuality,AirportDelays,PollenForecast,Breathing
 
-async def main():
-    print("Hello...")
-    await asyncio.sleep(1)
-    print("World!")
+"""
+CurrentConditions: Every 5 minutes
+Daily Forecasts, Hourlies, etc: 60 minutes
+Alerts: 5 minutes
+"""
+
+print("i2MessageEncoder-Python\nDeveloped by mewtek\nData record generators by Floppaa & Goldblaze")
+
+async def FiveMinUpdaters():
+    CurrentObservations.makeDataFile()
+    await asyncio.sleep(300)
 
 
-if __name__ == "__main__":
-    asyncio.run(main())
+async def HourUpdaters():
+    DailyForecast.makeDataFile()
+    HourlyForecast.makeDataFile()
+    AirQuality.writeData()
+    PollenForecast.makeDataFile()
+    AirportDelays.writeData()
+    Breathing.makeDataFile()
+    await asyncio.sleep(3600)
+
+loop = asyncio.get_event_loop()
+
+hourtasks = loop.create_task(HourUpdaters())
+fivemintasks = loop.create_task(FiveMinUpdaters())
+
+try:
+    loop.run_until_complete(hourtasks)
+    loop.run_until_complete(fivemintasks)
+except asyncio.CancelledError: pass
