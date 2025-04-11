29abf3fad6c3780a5d535b253be1927d6c23de32
mice <105393381+miceoroni@users.noreply.github.com>
2025-04-11 10:59:51 -0400

    Update PollenForecast.py

--- recordGenerators/PollenForecast.py
+++ recordGenerators/PollenForecast.py
@@ -30,7 +30,7 @@ for i in MPC.getPrimaryLocations():
 
 l.debug(pollenIds, geocodes)
 
-apiKey = '21d8a80b3d6b444998a80b3d6b1449d3'
+apiKey = 'e1f10a1e78da46f5b10a1e78da96f525'
 
 async def getData(pollenId, geocode):
     fetchUrl = f"https://api.weather.com/v2/indices/pollen/daypart/7day?geocode={geocode}&language=en-US&format=xml&apiKey={apiKey}"
